50b7f021d80af922b83aaba4a223af9f9cc65c57
Julian Gan <julian.cl.gan@gmail.com>
2019-10-08 12:25:59 -0700

    Updated survey_data with 2019 records

--- scripts/QC.R
+++ scripts/QC.R
@@ -13,20 +13,27 @@ fish <- read_csv(here("data", "fish_field_data.csv"), guess_max = 10000)
 sealice <- read_csv(here("data", "sealice_field.csv"))
 
 
-field_2019 <- gs_key("1cHgZszv--FlV207cwSpe9hFJipb7HhS4IW9vVrUNg8M", visibility = "private", lookup = FALSE)
-survey_2019 <- gs_read(field_2019, ws = "survey_data")
+# field_2019 <- gs_key("1cHgZszv--FlV207cwSpe9hFJipb7HhS4IW9vVrUNg8M", visibility = "private", lookup = FALSE)
+# survey_2019 <- gs_read(field_2019, ws = "survey_data")
 
-ctd <- survey_2019 %>% 
-  drop_na(ctd_drop) %>% 
-  mutate(hakai_id = paste(site_id, survey_date, ctd_drop, sep="_")) %>% 
-  select(survey_id, survey_date, site_id, hakai_id)
+survey_2019 <- survey_data %>% 
+  filter(survey_date > as.Date("2019-01-01")) %>% 
+  drop_na(ctd_bout) %>% 
+  mutate(hakai_id = paste(site_id, survey_date, ctd_bout, sep="_")) %>% 
+  select(survey_id, survey_date, site_id, hakai_id, secchi) %>% 
+  # Finding most mismatches are due to the wrong sampling bout being entered into the portal, so create a more general linkage of date_site
+  mutate(date_site = paste(survey_date, site_id, sep="_"))
 
 client <- hakaiApi::Client$new()
-endpoint = sprintf("%s/%s", client$api_root, 'eims/views/output/ctd_drops?date>=2019-01-01&date<2019-09-01&survey&&{"DISOCKEYE","JSSOCKEYE"}&limit=-1')
+endpoint = sprintf("%s/%s", client$api_root, 'eims/views/output/secchi?date>=2019-01-01&date<2019-09-01&survey&&{"DISOCKEYE","JSSOCKEYE"}&limit=-1')
 data <- client$get(endpoint)
 
-ctd_data <- full_join(ctd,data, by = "hakai_id")
+secchi <- data %>% 
+  mutate(date_site = paste(date, site_id, sep="_"))
 
-ctd_data_err <- ctd_data %>% 
-  filter(is.na(pk) | is.na(survey_id) & miscast != "TRUE")
+survey_secchi <- full_join(survey_2019,secchi, by = "date_site")
+
+data_err <- survey_secchi %>% 
+  # filter(is.na(pk) | is.na(survey_id) & miscast != "TRUE") # For CTD
+  filter(is.na(event_pk) | is.na(survey_id)) # For Secchi
 
