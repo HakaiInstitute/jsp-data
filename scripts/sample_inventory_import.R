70b46a624e63c3451b87d802a94d4d93983eb469
Julian Gan <julian.cl.gan@gmail.com>
2019-11-14 15:11:26 -0800

    Added analyzing_lab column to fatty_acid
    Also updated sample inventory import script to use new googlesheets4 package

--- scripts/sample_inventory_import.R
+++ scripts/sample_inventory_import.R
@@ -1,22 +1,31 @@
 library(tidyverse)
 library(here)
-library(googlesheets)
+library(googlesheets4)
 
 fish_f <- read_csv("https://raw.githubusercontent.com/HakaiInstitute/jsp-data/master/data/fish_field_data.csv", guess_max = 20000)
 fish_l <- read_csv("https://raw.githubusercontent.com/HakaiInstitute/jsp-data/master/data/fish_lab_data.csv", guess_max = 20000)
 fish <- full_join(fish_f, fish_l, by = "ufn")
 
-inv <- gs_key("1Ti5gGvakA4DUTjCUZ_VYHULU_FJCK05-zdly5E80Tzs", visibility = "private", lookup = FALSE)
-fa <- gs_read(inv, ws = "fa_metadata", col_types = c("cccccccccccc"))
-# fa <- read_csv(here("data", "unverified", "fatty_acid_samples.csv"), guess_max = 10000)
 
-fa_qc <- left_join(fish_l, fa, by = "ufn") %>% 
+
+fattyacid_gs <- read_sheet("1Ti5gGvakA4DUTjCUZ_VYHULU_FJCK05-zdly5E80Tzs", sheet = "fa_metadata")
+fa_ship_log <- read_sheet("1RF5yuH5bZj4fGrdXEdxgqCkr1MD2YaPX7UzS-MywhDQ", sheet = "fatty_acid")
+
+fa_qc <- left_join(fish_l, fattyacid_gs, by = "ufn") %>% 
   filter(is.na(sample_id))
 
-fa_fish <- left_join(fa, select(fish_l, ufn, date_processed), by = "ufn") %>% 
+fa_fish <- left_join(fattyacid_gs, select(fish_l, ufn, date_processed), by = "ufn") %>% 
   drop_na(date_processed) %>% 
   filter(sample_id != "UNKNOWN") %>% 
-  # filter(is.na(date_processed)) %>% 
-  select(ufn, sample_id, sample_comments = comments_sample)
+  mutate(analyzing_lab = ifelse(container_id %in% fa_ship_log$container_id, "JGarzke_UBC", NA),
+         quality_level = "raw",
+         quality_log = NA) %>% 
+  select(ufn,
+         sample_type,
+         sample_id,
+         sample_comments = comments_sample,
+         analyzing_lab,
+         quality_level,
+         quality_log)
 
 write_csv(fa_fish,here("data","samples", "fatty_acid.csv"))
