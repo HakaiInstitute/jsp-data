c7cd371db44c4b9466a126a0a70ba126682ac371
Julian Gan <julian.cl.gan@gmail.com>
2020-01-22 17:29:25 -0800

    Updated sample import script

--- scripts/elab_sample_upload.R
+++ scripts/elab_sample_upload.R
@@ -3,9 +3,12 @@ library(lubridate)
 library(googlesheets4)
 library(here)
 library(xlsx)
+library(gtools)
+library(naturalsort)
 
 fish_f <- read_csv("https://raw.githubusercontent.com/HakaiInstitute/jsp-data/master/data/fish_field_data.csv", guess_max = 20000)
 fish_l <- read_csv("https://raw.githubusercontent.com/HakaiInstitute/jsp-data/master/data/fish_lab_data.csv", guess_max = 10000)
+
 
 ## Carcasses -----------------------------------------------------------------------------------------------------------------------------
 carcass_gs <- read_sheet("1Ti5gGvakA4DUTjCUZ_VYHULU_FJCK05-zdly5E80Tzs", sheet = "carcass_metadata") %>% 
@@ -50,3 +53,74 @@ scale_elab <- inner_join(scale_gs,
   arrange(location, position)
 
 write_csv(scale_elab, here("elab_temp", "elab_scale_upload.csv"))
+
+
+## Otoliths  -----------------------------------------------------------------------------------------------------------------------------
+oto_gs <- read_sheet("1Ti5gGvakA4DUTjCUZ_VYHULU_FJCK05-zdly5E80Tzs", sheet = "otolith_metadata")
+oto_boxes <- read_csv("G:/Shared drives/Juvenile Salmon Program/Sample Management/eLab Container Upload/elab_jsp_otoliths.csv")
+oto_ship <- read_sheet("1RF5yuH5bZj4fGrdXEdxgqCkr1MD2YaPX7UzS-MywhDQ", sheet = "otolith")
+
+oto_db <- read_csv(here::here("data", "sample_inventory", "otolith_samples.csv"), guess_max = 10000) %>% 
+  left_join(select(oto_gs, ufn, container_id, container_cell)) %>% 
+  filter(ufn %in% fish_l$ufn &
+           container_cell != "NA") %>% 
+  mutate(location_qc = paste(container_id, container_cell, sep = "-")) %>% 
+  group_by(location_qc) %>% 
+  mutate(sample_quality_flag = case_when(n() != 1 ~ "SVC",
+                                         n() == 1 ~ "AV")) %>% 
+  ungroup() %>% 
+  mutate(analyzing_lab = ifelse(container_id %in% oto_ship$container_id, "UBC_Hunt", NA),
+         sample_quality_log = case_when(sample_quality_flag == "SVC" ~ paste("Cannot be verified. Transcribed duplicate location", location_qc))) %>% 
+  select(sample_id:analyzing_lab, sample_quality_flag, sample_quality_log)
+
+write_csv(oto_db, here("data", "sample_inventory", "otolith_samples.csv"))
+
+convert <- read_csv(here("elab_temp", "convert_5Ax20N.csv"))
+
+oto_elab <- inner_join(oto_gs,
+                       filter(oto_db,
+                              sample_quality_flag == "AV"),
+                       by = "sample_id") %>% 
+  left_join(oto_boxes, by = c("container_id" = "name")) %>% 
+  left_join(convert) %>% 
+  mutate(location = paste(destination, container_id, sep = " / "),
+         position = as.numeric(ifelse(str_detect(container_cell, "[:alpha:]"), elab,
+                                      container_cell))) %>% 
+  select(sample_id, position, location) %>% 
+  arrange(location, position)
+
+# oto_elab <- oto_elab[mixedorder(oto_elab$position),] %>% 
+#   arrange(location)
+
+write_csv(oto_elab, here("elab_temp", "elab_oto_upload.csv"))
+
+## Sea Lice Microbiome -----------------------------------------------------------------------------------------------------------------------------
+sl_gs <- read_sheet("1Ti5gGvakA4DUTjCUZ_VYHULU_FJCK05-zdly5E80Tzs", sheet = "sealice_metadata")
+slmb_gs <- sl_gs %>% 
+  filter(str_detect(sample_type, "cryo"))
+sl_ship <- read_sheet("1RF5yuH5bZj4fGrdXEdxgqCkr1MD2YaPX7UzS-MywhDQ", sheet = "sea_lice")
+
+slmb_db <- read_csv(here("data", "sample_inventory", "sea_lice_microbiome_samples.csv"), guess_max = 10000) %>%
+  full_join(select(slmb_gs,
+                   ufn, sample_id, container_id, container_cell)
+            ) %>% 
+  mutate(location_qc = paste(container_id, container_cell, sep = "-")) %>% 
+  group_by(location_qc) %>% 
+  mutate(sample_quality_flag = case_when(location_qc == "NA-NA" ~ "AV",
+                                         n() != 1 ~ "SVC",
+                                         n() == 1 ~ "AV")
+         ) %>% 
+  ungroup() %>%
+  arrange(location_qc) %>%
+  mutate(sample_type = "sealice_microbiome",
+         sample_subtype = case_when(str_detect(sample_id, "LC") ~ "mot_cal",
+                                    str_detect(sample_id, "LL") ~ "mot_lep",
+                                    str_detect(sample_id, "SL1") ~ "mot_pool"
+                                    ),
+         sample_quality_log = case_when(sample_quality_flag == "SVC" ~ paste("Cannot be verified. Transcribed duplicate location", location_qc)),
+         analyzing_lab = case_when(container_id %in% sl_ship$container_id ~ "UBC_Suttle", 
+                                   location_qc == "NA-NA" ~ "UBC_Suttle")
+         ) %>% 
+  select(sample_id, sample_type, sample_subtype, ufn, sample_comments, analyzing_lab, sample_quality_flag, sample_quality_log)
+
+write_csv(slmb_db, here("data", "sample_inventory", "sea_lice_microbiome_samples.csv"))
